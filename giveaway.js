03cfcd4f4983f222075b606c53ac993c11194a71
NAIR0M <116349996+NAIR0M@users.noreply.github.com>
2024-09-11 01:54:02 +0200

    3

--- giveaway.js
+++ giveaway.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name         participate
+// @name         giveaway
 // @namespace    http://tampermonkey.net/
 // @version      2024-09-10
 // @description  try to take over the world!
-// @author       You
+// @author       Nairom
+// @downloadURL  https://github.com/NAIR0M/instant-gaming-giveaway/raw/main/giveaway.js
+// @updateURL    https://github.com/NAIR0M/instant-gaming-giveaway/raw/main/giveaway.js
 // @match        *://www.instant-gaming.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=instant-gaming.com
 // @grant        GM_registerMenuCommand
@@ -14,28 +16,13 @@
 
   const jsonUrl = 'https://raw.githubusercontent.com/NAIR0M/instant-gaming-giveaway/main/giveawayList.json';
 
-  /*function participate() {
-    // Recherche du bouton avec les classes 'button' et 'validate'
-    const button = document.querySelector('button.button.validate');
-
-    if (button !== null) { // Vérifie si le bouton n'est pas désactivé
-      console.log('Bouton trouvé et actif, tentative de clic...');
-      button.click(); // Simuler le clic
-    }
-    return true;
-  }*/
-
-
   function openUrlSequentially(urls) {
     if (urls.length === 0) return;
 
     const url = urls.shift(); // Get the first URL
-    const newTab = window.open(url, '_blank'); // Open it in a new tab
 
-    newTab.onload = function () {
-      setTimeout(() => {
-      openUrlSequentially(urls); // Open the next URL
-    }, 1000);};
+    window.open(url, '_blank') // Open it in a new tab
+    openUrlSequentially(urls); // Open the next URL    
   }
 
   // Add a menu command to execute the script
